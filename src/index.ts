7659975eaec4a249e3f62eabd648307e16bd5e72
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-09-25 15:57:45 +0700

    feat: implement graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
-import config from "./config/env";
-import { errorHandler } from "./middlewares/error";
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import config from './config/env';
+import { errorHandler } from './middlewares/error';
 
 const app = new Hono();
 
 app.onError(errorHandler);
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
 });
 
-serve(
+const server = serve(
   {
     fetch: app.fetch,
     port: config.port,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
-  },
+  }
 );
+
+process.on('SIGINT', () => {
+  server.close();
+  process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+});
