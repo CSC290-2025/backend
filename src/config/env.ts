d40971f0306ebccc965733fc48c9b238684dc091
zersix225 <drago.overlond@gmail.com>
2025-09-28 19:48:48 +0700

    chore: add Firebase environment config

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,11 +5,28 @@ dotenv.config();
 interface Config {
   port: number;
   nodeEnv: string;
+
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
+
+  //Firebase
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.FIREBASE_DATABASE_URL!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.FIREBASE_APP_ID!,
 };
 
 export default config;
