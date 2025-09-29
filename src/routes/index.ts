9aba4bdcd3182f3b34812f75720332703db8e59e
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-09-29 11:36:50 +0700

    feat: implement openapi routes for users and update response util

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,23 @@
-export { userRoutes } from './user.routes';
+// export { userRoutes } from './user.routes';
+import type { OpenAPIHono } from '@hono/zod-openapi';
+import { setupUserRoutes } from './user.openapi.routes';
+import { createRoute } from '@hono/zod-openapi';
+import { z } from 'zod';
+
+// for future routes
+
+// import { setupAuthRoutes } from '@/modules/auth/auth.routes';
+// import { setupPaymentRoutes } from '@/modules/payment/payment.routes';
+// import { setupEmergencyRoutes } from '@/modules/emergency/emergency.routes';
+// etc, etc.
+
+export const setupRoutes = (app: OpenAPIHono) => {
+  setupUserRoutes(app);
+
+  // team modules:
+
+  // setupAuthRoutes(app);
+  // setupPaymentRoutes(app);
+  // setupEmergencyRoutes(app);
+  // etc, etc.
+};
