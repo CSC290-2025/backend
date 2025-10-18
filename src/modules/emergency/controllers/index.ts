bf61e74e0a4a14a33f32ce601c6c1e678d0d5c54
bonusshyn <shayanisshyn@gmail.com>
2025-10-18 12:34:43 +0700

    feat: add create report api

--- src/modules/emergency/controllers/index.ts
+++ src/modules/emergency/controllers/index.ts
@@ -1,2 +1,3 @@
 export * as FcmController from '@/modules/emergency/controllers/fcm.controller.ts';
 export * as TokenController from '@/modules/emergency/controllers/token.controller.ts';
+export * from './emergency.controller';
