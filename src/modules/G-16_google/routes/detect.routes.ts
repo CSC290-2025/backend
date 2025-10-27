c27c672593c8b4beb306e8807de3106af1b87820
kykkrm <nonlada.prem@mail.kmutt.ac.th>
2025-10-27 20:05:36 +0700

    rename folder to g-16_google

--- src/modules/G-16_google/routes/detect.routes.ts
+++ src/modules/G-16_google/routes/detect.routes.ts
@@ -1,9 +1,7 @@
 import { Hono } from 'hono';
-//import { detectHarmHandler } from '@/modules/_example/controllers/detect.controller';
 import { detectHarm } from '../controllers/detect.controller';
 
 export const detectRoutes = new Hono();
 
-// POST /ai/detect-harm  (multipart/form-data; field: image)
 detectRoutes.post('/detect-harm', detectHarm);
 // detectRoutes.get('/ping', (c) => c.text('detect ok'));
