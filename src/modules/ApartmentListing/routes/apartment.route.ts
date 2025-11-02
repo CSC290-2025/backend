38776e9089fafdb7aedc2a614e9f7939b87e1fd8
Nicha-h <nichahongsri@gmail.com>
2025-11-03 04:45:36 +0700

    fix: change to accept OpenAPI form

--- src/modules/ApartmentListing/routes/apartment.route.ts
+++ src/modules/ApartmentListing/routes/apartment.route.ts
@@ -1,12 +1,28 @@
-import { Hono } from 'hono';
 import * as apartmentController from '../controllers/apartment.Controller';
+import type { OpenAPIHono } from '@hono/zod-openapi';
+import { ApartmentSchemas } from '../schemas';
 
-const apartmentRoutes = new Hono();
+const setupApartmentRoutes = (app: OpenAPIHono) => {
+  app.openapi(
+    ApartmentSchemas.CreateApartmentRoute,
+    apartmentController.createApartment
+  );
+  app.openapi(
+    ApartmentSchemas.getApartmentbyIDRoute,
+    apartmentController.getApartmentByID
+  );
+  app.openapi(
+    ApartmentSchemas.getAllApartmentsRoute,
+    apartmentController.getAllApartment
+  );
+  app.openapi(
+    ApartmentSchemas.UpdateApartmentRoute,
+    apartmentController.updateApartment
+  );
+  app.openapi(
+    ApartmentSchemas.DeleteApartmentRoute,
+    apartmentController.deleteApartment
+  );
+};
 
-apartmentRoutes.get('/:id', apartmentController.getApartmentByID);
-apartmentRoutes.get('/', apartmentController.getAllApartment);
-apartmentRoutes.post('/', apartmentController.createApartment);
-apartmentRoutes.patch('/:id', apartmentController.updateApartment);
-apartmentRoutes.delete('/:id', apartmentController.deleteApartment);
-
-export { apartmentRoutes };
+export { setupApartmentRoutes };
