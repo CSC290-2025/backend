07b440e120b01c6ca56ea65eaa0340636f8343e9
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-11-03 02:39:51 +0700

    feat: add routes for creating qr code

--- src/modules/Financial/routes/scb.openapi.routes.ts
+++ src/modules/Financial/routes/scb.openapi.routes.ts
@@ -1,1 +1,9 @@
-// For future SCB-related endpoints
+import { ScbController } from '../controllers';
+import { ScbSchemas } from '../schemas';
+import type { OpenAPIHono } from '@hono/zod-openapi';
+
+const setupScbRoutes = (app: OpenAPIHono) => {
+  app.openapi(ScbSchemas.createQrRoute, ScbController.createQrCode);
+};
+
+export { setupScbRoutes };
