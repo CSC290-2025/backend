c5b70af11144a92996ae975df90c9572f2a1b032
JedZBeach <audphone928@gmail.com>
2025-11-02 23:31:08 +0700

    draft

--- src/modules/weather/index.ts
+++ src/modules/weather/index.ts
@@ -2,5 +2,4 @@ export type * from './types';
 export { WeatherSchemas } from './schemas';
 export { WeatherModel } from './models';
 export { WeatherService } from './services';
-
-// export { setupWeatherRoutes } from './routes';
+export { setupWeatherRoutes } from './routes';
