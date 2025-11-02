5104d48132182bbbb497040c01e84c49a5a9f70c
Theerawat Patthawee <smrmakeyt@gmail.com>
2025-11-02 16:50:06 +0700

    refactor: Removed warning prisma generate as it interfere with typescript

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,25 +1,6 @@
+import { PrismaClient } from '@/generated/prisma';
 import { withAccelerate } from '@prisma/extension-accelerate';
-import type { PrismaClient as BasePrismaClient } from '@/generated/prisma';
 
-type AcceleratedPrismaClient = ReturnType<BasePrismaClient['$extends']>;
-
-let prisma: AcceleratedPrismaClient;
-
-try {
-  const { PrismaClient } = await import('@/generated/prisma');
-  prisma = new PrismaClient().$extends(withAccelerate());
-} catch (_err) {
-  console.warn(
-    [
-      'Prisma client not found.',
-      'You likely need to run: `pnpx prisma generate`',
-      'If this is a fresh clone, also ensure `.env` is configured correctly.',
-    ].join('\n')
-  );
-
-  throw new Error(
-    "Prisma client not found. Please run 'pnpx prisma generate' before starting the server."
-  );
-}
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export default prisma;
