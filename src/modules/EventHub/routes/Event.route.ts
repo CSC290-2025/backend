b4fa8b2655ace513ce6fcd5eda19f837c71e2400
Pal <thanapat.doun@mail.kmutt.ac.th>
2025-11-02 19:41:44 +0700

    feat: add auth to only admin can post update delete post

--- src/modules/EventHub/routes/Event.route.ts
+++ src/modules/EventHub/routes/Event.route.ts
@@ -1,6 +1,7 @@
 import type { OpenAPIHono } from '@hono/zod-openapi';
 import { EventSchemas } from '../schemas';
 import { EventController } from '../controllers';
+import { AuthMiddleware } from '@/middlewares/auth_example';
 
 const setupEventRoutes = (app: OpenAPIHono) => {
   // Event Routes
@@ -11,14 +12,23 @@ const setupEventRoutes = (app: OpenAPIHono) => {
   // Get a single event by ID (public)
   app.openapi(EventSchemas.getEventRoute, EventController.getEvent);
 
-  // Create a new event (auth required)
-  app.openapi(EventSchemas.createEventRoute, EventController.createEvent);
+  // Create a new event (admin only)
+  app.openapi(EventSchemas.createEventRoute, async (c) => {
+    await AuthMiddleware.isAdmin(c, async () => {});
+    return EventController.createEvent(c);
+  });
 
-  // Update existing event (auth)
-  app.openapi(EventSchemas.updateEventRoute, EventController.updateEvent);
+  // Update existing event (admin only)
+  app.openapi(EventSchemas.updateEventRoute, async (c) => {
+    await AuthMiddleware.isAdmin(c, async () => {});
+    return EventController.updateEvent(c);
+  });
 
-  // Delete event (auth)
-  app.openapi(EventSchemas.deleteEventRoute, EventController.deleteEvent);
+  // Delete event (admin only)
+  app.openapi(EventSchemas.deleteEventRoute, async (c) => {
+    await AuthMiddleware.isAdmin(c, async () => {});
+    return EventController.deleteEvent(c);
+  });
 
   // Get daily event count (analytics or calendar view)
   app.openapi(
