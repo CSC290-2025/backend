351799901e83065271b51e37eaf888aaca1f9c61
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-11-02 17:12:27 +0700

    fix: better error

--- src/modules/Financial/services/scb.service.ts
+++ src/modules/Financial/services/scb.service.ts
@@ -1,4 +1,5 @@
 import { ScbModel } from '../models';
+import { ValidationError } from '@/errors';
 import type { ScbToken } from '../types';
 
 // the same as the one from model no business logic added
@@ -10,10 +11,12 @@ const encrypt = async (data: object): Promise<string> => {
   const jsonString = JSON.stringify(data);
   const PUBLIC_KEY = process.env.G11_PUBLIC_KEY;
   if (!PUBLIC_KEY) {
-    throw new Error('G11_PUBLIC_KEY environment variable is not defined');
+    throw new ValidationError(
+      'G11_PUBLIC_KEY environment variable is not defined'
+    );
   }
-  if (data === null || data === undefined) {
-    throw new Error('Data to encrypt cannot be null or undefined');
+  if (!data) {
+    throw new ValidationError('Data to encrypt cannot be null or undefined');
   }
   return await ScbModel.encryptData(jsonString, PUBLIC_KEY);
 };
@@ -21,10 +24,12 @@ const encrypt = async (data: object): Promise<string> => {
 const decrypt = async (encryptedData: string): Promise<object> => {
   const PUBLIC_KEY = process.env.G11_PUBLIC_KEY;
   if (!PUBLIC_KEY) {
-    throw new Error('G11_PUBLIC_KEY environment variable is not defined');
+    throw new ValidationError(
+      'G11_PUBLIC_KEY environment variable is not defined'
+    );
   }
   if (!encryptedData) {
-    throw new Error('Encrypted data cannot be null or undefined');
+    throw new ValidationError('Encrypted data cannot be null or undefined');
   }
   const decryptedString = await ScbModel.decryptData(encryptedData, PUBLIC_KEY);
   return JSON.parse(decryptedString);
