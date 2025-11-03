926533fff100d9d1d309e4a1ac7fdde3950ca9ac
SittaWetpa <sitta.wetpa@mail.kmutt.ac.th>
2025-11-03 09:46:18 +0700

    feat: add traffic route

--- src/modules/traffic/routes/index.ts
+++ src/modules/traffic/routes/index.ts
@@ -1,22 +1,2 @@
-import type { OpenAPIHono } from '@hono/zod-openapi';
-import { LightRequestSchemas, VehicleSchemas } from '../schemas';
-import { LightRequestController, VehicleController } from '../controllers';
-
-export const setupTrafficRoutes = (app: OpenAPIHono) => {
-  // Light Request Routes
-  app.openapi(
-    LightRequestSchemas.createRequestRoute,
-    LightRequestController.createLightRequest
-  );
-  app.openapi(
-    LightRequestSchemas.getRequestsRoute,
-    LightRequestController.getLightRequests
-  );
-
-  // Vehicle Routes
-  app.openapi(
-    VehicleSchemas.updateLocationRoute,
-    VehicleController.updateVehicleLocation
-  );
-  app.openapi(VehicleSchemas.getVehicleRoute, VehicleController.getVehicle);
-};
+export { setupTrafficRoutes } from './light-request.openapi.routes';
+//export {setupTrafficRoutes} from './traffic.openapi.routes'
