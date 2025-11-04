28f040f73c7416bbb61ded7d8225db2cce10c4bc
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-11-05 01:32:26 +0700

    feat: add ScbApiQrRequest and ScbQrConfirmResponse types

--- src/modules/Financial/types/scb.types.ts
+++ src/modules/Financial/types/scb.types.ts
@@ -5,10 +5,25 @@ type ScbToken = z.infer<typeof ScbSchemas.ScbTokenSchema>;
 type ScbQrRequestSchema = z.infer<typeof ScbSchemas.ScbQrRequestSchema>;
 type ScbQrResponseSchema = z.infer<typeof ScbSchemas.ScbQrResponseSchema>;
 type ScbWebhookPayload = z.infer<typeof ScbSchemas.ScbWebhookPayloadSchema>;
+type ScbQrConfirmResponse = z.infer<
+  typeof ScbSchemas.ScbQrConfirmResponseSchema
+>;
+
+type ScbApiQrRequest = {
+  qrType: string;
+  ppType: string;
+  ppId: string;
+  amount: string;
+  ref1: string;
+  ref2: string;
+  ref3: string;
+};
 
 export type {
   ScbToken,
   ScbQrRequestSchema,
   ScbQrResponseSchema,
   ScbWebhookPayload,
+  ScbQrConfirmResponse,
+  ScbApiQrRequest,
 };
