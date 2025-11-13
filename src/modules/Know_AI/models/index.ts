aee67ebb63ed2f3b4c6133847b9c133a7785d328
ChocoPie <worapratch06@gmail.com>
2025-11-13 20:53:30 +0700

    feat: CRUD courses

--- src/modules/Know_AI/models/index.ts
+++ src/modules/Know_AI/models/index.ts
@@ -1,6 +1,6 @@
 export * as EnrollmentModel from './enrollment.model';
 export * as CourseModel from './course.model';
-export * as OnsiteSessionModel from './onsite.model';
+// export * as OnsiteSessionModel from './onsite.model';
 export * as QuestionModel from './question.model';
 export * as LevelModel from './level.model';
 export * as ExerciseModel from './exercise.model';
