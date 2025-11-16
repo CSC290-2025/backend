5b5f97a5accd68b7162565ef0e7e8aee3a4d2485
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-11-16 12:18:23 +0700

    feat: add auth middleware support

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,23 @@
+import { UnauthorizedError } from '@/errors';
+import type { MiddlewareHandler } from 'hono';
+import { getCookie } from 'hono/cookie';
+import { verifyAccessToken } from '@/utils/jwt';
+
+const authMiddleware: MiddlewareHandler = async (c, next) => {
+  const accessToken = getCookie(c, 'accessToken');
+
+  if (!accessToken) {
+    throw new UnauthorizedError('Missing access token, please login');
+  }
+
+  const payload = await verifyAccessToken(accessToken);
+
+  if (!payload) {
+    throw new UnauthorizedError('Invalid or expired access token');
+  }
+
+  c.set('user', payload);
+  await next();
+};
+
+export { authMiddleware };
