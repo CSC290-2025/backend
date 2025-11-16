1dc869c1178c125a755af2a196510c54ceb11a7b
NPC-PATRICK <patnpc1603@gmail.com>
2025-11-16 16:42:15 +0700

    feat(traffic): Add esp32 code

--- src/modules/traffic/schemas/index.ts
+++ src/modules/traffic/schemas/index.ts
@@ -1,6 +1,8 @@
 export { TrafficLightSchemas } from './traffic-light.schemas';
 export { LightRequestSchemas } from './light-request.schemas';
 export { VehicleSchemas } from './vehicle.schemas';
+export { TrafficEmergencySchemas } from './traffic_emergencies.schemas';
+export { RoadSchemas } from './roads.schemas';
 export { IntersectionSchemas } from './intersection.schemas';
 export { RoadSchemas } from './roads.schemas';
 export { TrafficEmergencySchemas } from './traffic_emergencies.schemas';
