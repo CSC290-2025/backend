7e0a16c444e4bdf72afa2b407e2e7fc002c43a30
Aung Moe Lin <aung.moel@kmutt.ac.th>
2025-11-18 15:45:26 +0700

    Some bug fixes in insurance page

--- src/modules/Financial/routes/index.ts
+++ src/modules/Financial/routes/index.ts
@@ -2,4 +2,3 @@ export { setupWalletRoutes } from './wallet.openapi.routes';
 export { setupScbRoutes } from './scb.openapi.routes';
 export { setupInsuranceCardRoutes } from './insurance-card.openapi.routes';
 export { setupMetroCardRoutes } from './metroCard.openapi.routes';
-export { setupInsuranceCardRoutes } from './insurance-card.openapi.routes';
