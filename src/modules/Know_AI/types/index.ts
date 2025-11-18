12a4b4d2985efed6ccc077ffbcf31fc182f1804f
ChocoPie <worapratch06@gmail.com>
2025-11-18 13:23:27 +0700

    chore: clear unused feature

--- src/modules/Know_AI/types/index.ts
+++ src/modules/Know_AI/types/index.ts
@@ -1,6 +1,5 @@
 export * from './enrollment.type';
 export * from './course.type.ts';
-export * from './onsite.type';
 export * from './question.type.ts';
 export * from './level.type.ts';
 export * from './exercise.type.ts';
