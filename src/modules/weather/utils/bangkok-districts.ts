dadc3654f75c3ebd439c24391c4ce19a4fa1d68e
Jedsarit Fanpimiy <audphone928@gmail.com>
2025-11-19 16:44:51 +0700

    database connected address

--- src/modules/weather/utils/bangkok-districts.ts
+++ src/modules/weather/utils/bangkok-districts.ts
@@ -1,13 +1,32 @@
 export const bangkokDistricts = [
   {
     location_id: 1,
+    address_id: 16,
     name: 'Thung Khru',
     lat: 13.632160030710809,
     lng: 100.4911907279456,
   },
-  { location_id: 2, name: 'Rat Burana', lat: 13.684452, lng: 100.6 },
-  { location_id: 3, name: 'Thon Buri', lat: 13.720639, lng: 100.488936 },
-  { location_id: 4, name: 'Chom Thong', lat: 13.687762, lng: 100.477365 },
+  {
+    location_id: 2,
+    address_id: 17,
+    name: 'Rat Burana',
+    lat: 13.684452,
+    lng: 100.6,
+  },
+  {
+    location_id: 3,
+    address_id: 18,
+    name: 'Thon Buri',
+    lat: 13.720639,
+    lng: 100.488936,
+  },
+  {
+    location_id: 4,
+    address_id: 19,
+    name: 'Chom Thong',
+    lat: 13.687762,
+    lng: 100.477365,
+  },
 ];
 
 export const getDistrictByLocationId = (locationId: number) => {
