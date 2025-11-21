ac5c378bfa50c261a02b3a1139cd255a122c4dbc
Slade <saib6064@gmail.com>
2025-11-21 12:35:58 +0700

    fix(config/env.ts): set default jwtRefreshExpiresIn to 7 days feat(config/env.ts): load configuration from environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,39 +3,39 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface Config {
-port: number;
-nodeEnv: string;
-cloudinary_name: string;
-cloudinary_api_key: string;
-cloudinary_api_secret: string;
-isProduction: boolean;
-jwtSecret: string;
-jwtRefreshSecret: string;
-jwtExpiresIn: string;
-jwtRefreshExpiresIn: string;
-metroCardEncryptionKey: string;
-metroCardHashKey: string;
-adminRoleId: number;
+  port: number;
+  nodeEnv: string;
+  cloudinary_name: string;
+  cloudinary_api_key: string;
+  cloudinary_api_secret: string;
+  isProduction: boolean;
+  jwtSecret: string;
+  jwtRefreshSecret: string;
+  jwtExpiresIn: string;
+  jwtRefreshExpiresIn: string;
+  metroCardEncryptionKey: string;
+  metroCardHashKey: string;
+  adminRoleId: number;
 }
 
 const config: Config = {
-port: Number(process.env.PORT) || 3000,
-nodeEnv: process.env.NODE_ENV || 'development',
+  port: Number(process.env.PORT) || 3000,
+  nodeEnv: process.env.NODE_ENV || 'development',
 
-cloudinary_name: process.env.CLOUDINARY_NAME || '',
-cloudinary_api_key: process.env.CLOUDINARY_API_KEY || '',
-cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET || '',
-isProduction: process.env.NODE_ENV === 'production',
+  cloudinary_name: process.env.CLOUDINARY_NAME || '',
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY || '',
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET || '',
+  isProduction: process.env.NODE_ENV === 'production',
 
-jwtSecret: process.env.JWT_SECRET || 'secret-key',
-jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || 'secret-key',
-jwtExpiresIn: process.env.JWT_EXPIRES_IN || '15m',
-jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+  jwtSecret: process.env.JWT_SECRET || 'secret-key',
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || 'secret-key',
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '15m',
+  jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7',
 
-metroCardEncryptionKey: process.env.G11_CARD_ENC_KEY || 'secret-key',
-metroCardHashKey: process.env.G11_CARD_HASH_KEY || 'secret-key',
+  metroCardEncryptionKey: process.env.G11_CARD_ENC_KEY || 'secret-key',
+  metroCardHashKey: process.env.G11_CARD_HASH_KEY || 'secret-key',
 
-adminRoleId: Number(process.env.ADMIN_ROLE_ID) || 1
+  adminRoleId: Number(process.env.ADMIN_ROLE_ID) || 1,
 };
 
 export default config;
