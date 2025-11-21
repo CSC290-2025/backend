9403c744ee2a6de63b8110c5521c51647d2d1018
Nang Hayman Aye Mya <haymanayemya@gmail.com>
2025-11-21 07:37:33 +0700

    fix(power-bi): adapt role naming and remove admin middleware for reports.

--- src/modules/power-bi/controllers/reports.controller.ts
+++ src/modules/power-bi/controllers/reports.controller.ts
@@ -14,7 +14,15 @@ const getAllReports = async (c: Context) => {
 // Reports listing (metadata-driven across categories, filtered by role)
 const getReports = async (c: Context) => {
   const user = c.get('user');
-  const role = user.roleId === ADMIN_ROLE_ID ? 'admin' : 'citizens';
+  console.log('Getting reports by role', user);
+  // const role = user.roleId === ADMIN_ROLE_ID ? 'admin' : 'citizen';
+
+  // Temp fix for role naming difference
+  // will use citizen later
+  let role = user.role_name.toLowerCase();
+  if (role === 'citizen') {
+    role = 'citizens';
+  }
   const queryRole = c.req.query('role');
 
   if (queryRole != role) {
