c6a3e658faa8fc9dcbc955191dc9eef9d1db2f4d
Nicha-h <nichahongsri@gmail.com>
2025-11-23 00:12:32 +0700

    feat: add owner CRUD

---
+++ src/modules/ApartmentListing/controllers/owner.controller.ts
@@ -0,0 +1,15 @@
+import { ownerService } from '../service';
+import type { Context } from 'hono';
+import { successResponse } from '@/utils/response';
+
+export async function getApartmentOwnerByApartmentId(c: Context) {
+  const apartment_id = Number(c.req.param('id'));
+  const owners =
+    await ownerService.getApartmentOwnerByApartmentId(apartment_id);
+  return successResponse(c, owners, 200);
+}
+
+export async function getOwnerRole(c: Context) {
+  const roles = await ownerService.getOwnerRole();
+  return successResponse(c, roles, 200);
+}
