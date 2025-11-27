30dd971653f8b725065234a7205471e15a1fc888
sorasit <sorasit>
2025-11-27 17:09:48 +0700

    tap function

--- src/modules/public-transportation/routes/digital_card.route.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono';
-import { handleCreateDigitalCard } from '../controllers/digital_card.controller';
-
-const cardRoute = new Hono();
-
-cardRoute.post('/card', handleCreateDigitalCard);
-
-export default cardRoute;
--- src/modules/public-transportation/routes/transportation_topup.route.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from 'hono';
-import { handleTopUp } from '../controllers/transportation_topup.controller';
-const topUpRoute = new Hono();
-
-topUpRoute.post('/transactions/topup', handleTopUp);
-
-export default topUpRoute;
