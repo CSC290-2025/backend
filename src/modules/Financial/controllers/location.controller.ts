54981a790f882610b64eb4459371994066d2366c
Mykal-Steele <mykal.stele@gmail.com>
2025-12-16 01:40:26 +0700

    fix: use successResponse

--- src/modules/Financial/controllers/location.controller.ts
+++ src/modules/Financial/controllers/location.controller.ts
@@ -1,10 +1,11 @@
 import { LocationService } from '../services';
 import type { Context } from 'hono';
+import { successResponse } from '@/utils/response';
 
 const getNearbyPlaces = async (c: Context) => {
   const query = c.req.query();
   const response = await LocationService.getNearbyPlaces(query);
-  return c.json(response, 200);
+  return successResponse(c, { places: response });
 };
 
 export { getNearbyPlaces };
