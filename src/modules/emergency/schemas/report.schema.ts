6f9b64c1f5e76ae34657bf54b865dcba62f8e946
zersix225 <drago.overlond@gmail.com>
2025-12-17 21:03:01 +0700

    chore: fix schema in report

--- src/modules/emergency/schemas/report.schema.ts
+++ src/modules/emergency/schemas/report.schema.ts
@@ -10,12 +10,8 @@ const CreateReportSchema = z.object({
   title: z.string(),
   image_url: z.string().optional().nullable(),
   description: z.string().min(5).max(1000),
-  location: z
-    .object({
-      lat: z.number().min(-90).max(90),
-      lng: z.number().gte(-180).lte(180),
-    })
-    .optional(),
+  lat: z.string().nullable(),
+  long: z.string().nullable(),
   ambulance_service: z.boolean().default(false),
   report_category: z
     .enum(['traffic', 'accident', 'disaster'])
@@ -28,7 +24,8 @@ const ReportResponseSchema = z.object({
   user_id: z.number().int().nullable().optional(),
   image_url: z.string().nullable(),
   description: z.string().min(5).max(1000).nullable(),
-  location: z.any().nullable().optional(),
+  lat: z.string().nullable(),
+  long: z.string().nullable(),
   ambulance_service: z.boolean().nullable(),
   contact_center_service: z.boolean().nullable().optional(),
   level: z
